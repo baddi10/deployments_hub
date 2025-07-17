c0f9b4adbbe76791d290754fc0e076aa67f01aea
Baddi <cvremo69@gmail.com>
2025-07-17 17:05:42 +0530

    Deployments from Github actions to Render

---
+++ src/main/java/com/example/first_jar/FirstJarApplication.java
@@ -0,0 +1,23 @@
+package com.example.first_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@Controller
+public class FirstJarApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstJarApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index"; // maps to src/main/resources/templates/index.html
+	}
+
+}
---
+++ src/test/java/com/example/first_jar/FirstJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
